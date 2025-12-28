b1f69f82db999cd959498dd2163375d259c9a85d
Vladyslav <shv.vladyslav@gmail.com>
2025-12-28 02:04:32 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,41 @@
+/* K2 CamperBox PWA SW - simple offline cache */
+const CACHE = "k2camperbox-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/sw.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(req, copy)).catch(()=>{});
+        return res;
+      }).catch(() => caches.match("/index.html"));
+    })
+  );
+});
 
